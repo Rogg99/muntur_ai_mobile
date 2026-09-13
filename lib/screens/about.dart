48e59f7373d16778dacc4ce11b3bc6fe6e8b3ff4
francketape <franck.etape@sankoftechnologies.com>
2026-09-13 01:26:44 +0100

    Update About screen copyright to match AUTOSYNX BY ZEMNOVA corporate identity

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -81,7 +81,7 @@ class AboutState extends State<About>  with TickerProviderStateMixin{
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Flexible(child: Text(
-                    'Copyright © 2025 by \n AUTOSYNX Inc.',
+                    'Copyright © 2025 \nAUTOSYNX BY ZEMNOVA',
                   textAlign: TextAlign.center,
                   style: appStyle.H4(),
                 ))
